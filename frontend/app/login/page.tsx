1d168447592d8c428f0c5e551b1aa9afb00dbc75
FrankYin <djmax96945147@outlook.com>
2024-04-24 01:25:26 +0200

    [feat] Frontend add Google/Github login auth. cyberpunk css

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,35 @@
+// app/login/page.tsx
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Login() {
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      redirect("/admin");
+    }
+  }, [session]);
+
+  return (
+    <div style={{
+      textAlign: 'center',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <p style={{ fontSize: '1.5rem' }}>This is the login page - public route</p>
+      <button className="github" onClick={() => signIn("github")}>
+        Sign in with GitHub
+      </button>
+      <button className="google" onClick={() => signIn("google")}>
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
