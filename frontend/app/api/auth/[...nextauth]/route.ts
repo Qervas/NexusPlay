638d546dae730f73ba84257e166a297f54ea0a9f
FrankYin <djmax96945147@outlook.com>
2024-04-29 04:27:06 +0200

    [fix] ts export fix for routing

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,9 @@
 // app/api/auth/[...nextauth]/route.ts
 
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
-
-const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GG_ID as string,
-      clientSecret: process.env.GG_SECRET as string,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-};
+import authOptions from '../../../config/authConfig'; // Adjust the path as necessary
 
 const authHandler = NextAuth(authOptions);
 
 export const GET = authHandler;
 export const POST = authHandler;
-export default authOptions;
---
+++ frontend/app/config/authConfig.js
@@ -0,0 +1,17 @@
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GG_ID,
+      clientSecret: process.env.GG_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
+
+export default authOptions;
